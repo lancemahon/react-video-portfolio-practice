a9f236283b921d35ec52b8ccc9f31d06ecfb032c
Lance Mahon <lance.mahon@gmail.com>
2020-03-26 18:41:07 -0400

    After mucking around with useState to manage my overall app state, I wised up and went to useReducer
    - created a reducer file with the following actions:
      - 'wants-to-add' - for coditionall rendering AddVideoForm versus
      just a button
      - 'add-video' to add a video to state with a unique id
      - 'update-video' to change the url
      - 'delete-video' to remove it from state

---
+++ src/reducer.js
@@ -0,0 +1,32 @@
+function reducer(state, action) {
+  switch (action.type) {
+    case 'wants-to-add':
+      return {
+        ...state,
+        wantsToAdd: true
+      }
+    case 'add-video':
+      console.log('action.url: ' + action.url)
+      console.log('action.id: ' + action.id)
+      return {
+        wantsToAdd: false,
+        videos: [...state.videos, { url: action.url, id: action.id}],
+      }
+    case 'update-video':
+      return {
+        ...state,
+        videos: state.videos.map(v => v.id === action.id ?
+          {url: action.url, id: action.id} : v
+        )
+      }
+    case 'delete-video':
+      return {
+        ...state,
+        videos: state.videos.filter(v => v.id !== action.id)
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
