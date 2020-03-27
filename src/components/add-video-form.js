a4ec35fd3b94c11db331bb414f382a49f22a7de7
Lance Mahon <lance.mahon@gmail.com>
2020-03-26 22:43:16 -0400

    I'm using 'valid-url' to help me validate urls in AddVideoForm. It's not perfect, since I don't make sure the url can be displayed with ReactPlayer. But I have stopped updating state with non-url strings, sending some UI feedback instead.

--- src/components/add-video-form.js
+++ src/components/add-video-form.js
@@ -1,19 +1,38 @@
 import React, { useState, useContext } from 'react'
 import {Context} from '../Context'
+import { isWebUri } from 'valid-url'
+import styled from 'styled-components'
+
 
 function AddVideoForm() {
   const {addVideo} = useContext(Context)
   const [textState, setTextState] = useState('')
+  const [isWrongUrl, setIsWrongUrl] = useState(false)
 
   function handleChange(e) {
     setTextState(e.target.value)
   }
 
+  const StyledP = styled.p`
+    display: ${prop => prop.active ? 'initial' : 'none'};
+  `
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!isWebUri(textState)) {
+          setIsWrongUrl(true)
+        }
+    else addVideo(textState)
+  }
+
   return (
-    <form onSubmit={() => addVideo(textState)}>
-      <input type='text' placeholder='Video url' onChange={handleChange} />
-      <input type='submit' value='Submit' />
-    </form>
+    <div>
+      <StyledP active={isWrongUrl}>Please enter a valid url</StyledP>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Video url' onChange={handleChange} />
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
   )
 }
 
