e9dce9967739c0265086ba36f9377fb9ea840769
Lance Mahon <lance.mahon@gmail.com>
2020-03-26 22:47:32 -0400

    Made a delete button, it works!
    - It gets the id from props via VideoBox
    - called deleteVideo, gets it from Context

---
+++ src/components/delete-button.js
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react'
+import {Context} from '../Context'
+
+function DeleteButton ({id}) {
+  const {deleteVideo} = useContext(Context)
+
+  return (
+    <button onClick={() => {
+      console.log('Clicked delete button, id is: ' + id)
+      deleteVideo(id)}}>Delete</button>
+  )
+}
+
+export default DeleteButton
