7c8a31df8a41b26ac41c453c45f62fe4774cc421
Lance Mahon <lance.mahon@gmail.com>
2020-03-28 14:15:25 -0400

    AddVideoForm has reduced responsibility now
    - Styling is pulled into AddVideoFormContainer
    - So is the DeleteButton

--- src/components/add-video-form.js
+++ src/components/add-video-form.js
@@ -1,31 +1,23 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect, useRef } from 'react'
 import {Context} from '../Context'
 // import styled from 'styled-components'
 import ReactPlayer from 'react-player'
 import StyledP from './styled-p-tag'
 import Button from 'react-bootstrap/Button'
-import { Grid, Row, Col } from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
 
-import styled from 'styled-components'
 import { Form } from 'react-bootstrap'
 
-const FormWrapper = styled.section`
-  background: #383c44;
-  min-height: 10vh;
-  border-radius: 10px;
-  display: grid;
-  grid-template-columns: 150px 1fr 150px;
-`
-const FormInnards = styled.section`
-  border-radius: 10px;
-  grid-column: 2;
-  align-self: center;
-`
-
 function AddVideoForm() {
-  const {state, wantsToAdd, addVideo} = useContext(Context)
+  const {addVideo} = useContext(Context)
   const [textState, setTextState] = useState('')
   const [isWrongUrl, setIsWrongUrl] = useState(false)
+
+  const urlInput = useRef(null)
+
+  useEffect(() => {
+    urlInput.current.focus()
+  }, [])
 
   function handleChange(e) {
     setTextState(e.target.value)
@@ -43,31 +35,21 @@ function AddVideoForm() {
 
   return (
     <div>
-      <FormWrapper>
-        <FormInnards>
-          {state.wantsToAdd ?  
-          <div>
-            <StyledP active={isWrongUrl}>Please enter a valid url</StyledP>
-            <form onSubmit={handleSubmit}>
-              <Form.Group controlId='formAddVideo'>
-                <Form.Row>
-                  <Col sm={10}>
-                    <Form.Control type='input' placeholder='Video url' onChange={handleChange} />
-                  </Col>
-                    {/*<input type='text' placeholder='Video url' onChange={handleChange} />*/}
-                    <Col sm={1}>
-                      <Button variant='primary' type='submit'>
-                        Submit
-                      </Button>
-                    </Col>
-                  </Form.Row>
-                  {/*<input type='submit' value='Submit' />*/}
-              </Form.Group>
-            </form>
-          </div> :
-          <Button variant='primary' onClick={() => wantsToAdd()}>Add a video</Button>}
-        </FormInnards>
-      </FormWrapper>
+      <StyledP active={isWrongUrl}>Please enter a valid url</StyledP>
+      <form onSubmit={handleSubmit}>
+        <Form.Group controlId='formAddVideo'>
+          <Form.Row>
+            <Col sm={10}>
+              <Form.Control type='input' placeholder='Video url' ref={urlInput} onChange={handleChange} />
+            </Col>
+              <Col sm={1}>
+                <Button variant='primary' type='submit'>
+                  Submit
+                </Button>
+              </Col>
+            </Form.Row>
+        </Form.Group>
+      </form>
     </div>
   )
 }
