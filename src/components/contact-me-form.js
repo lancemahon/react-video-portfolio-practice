b5459b3171278156ad75dc103ff6e721d61a7878
Lance Mahon <lance.mahon@gmail.com>
2020-04-02 15:32:02 -0400

    I built a ContactMeForm that uses axios to make a post request.

--- src/components/contact-me-form.js
+++ src/components/contact-me-form.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import emailjs from 'emailjs-com';
+import axios from 'axios'
 
 function ContactMeForm () {
 
   function sendEmail(e) {
+    console.log('inside sendEmail')
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'contact_form', e.target, 'user_VKVrgr5BwaDr4EGM4EizX')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('assigned name, email and message')
+    axios({
+        method: "POST", 
+        url:"http://localhost:3001/send", 
+        data: {
+            name: name,   
+            email: email,  
+            messsage: message
+        }
+    }).then((response)=>{
+        if (response.data.msg === 'success'){
+            alert("Message Sent."); 
+            document.getElementById('contact-form').reset();
+          }else if(response.data.msg === 'fail'){
+            alert("Message failed to send.")
+        }
+    })
   }
 
   return (
-    <form className="contact-form" onSubmit={sendEmail}>
+    <form className="contact-form" id='contact-form' onSubmit={sendEmail}>
       <input type="hidden" name="contact_number" />
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="user_name" id='name' />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="user_email" id='email' />
       <label>Message</label>
-      <textarea name="message" />
+      <textarea name="message" id='message' />
       <input type="submit" value="Send" />
     </form>
   );
