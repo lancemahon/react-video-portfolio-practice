d58e812e4e71f63d35d61a6dfccbab68fac8bdab
Lance Mahon <lance.mahon@gmail.com>
2020-04-05 17:19:08 -0400

    Removed console log

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -25,9 +25,7 @@ router.post('/send', (req, res, next) => {
   var name = req.body.name
   var email = req.body.email
   var message = req.body.message
-  var content = `name: ${name} \n email: ${email} \n message: ${content} `
-
-  console.log('name, email, and message are: ' + name + email + message)
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `
 
   var mail = {
     from: name,
